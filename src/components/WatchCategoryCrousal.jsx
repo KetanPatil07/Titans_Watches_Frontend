55fa0afd88307b76af7606e306cd2d1ae5839759
KetanPatil07 <patilketan1410@gmail.com>
2025-07-01 16:58:43 +0530

    watchcategory

--- src/components/WatchCategoryCrousal.jsx
+++ src/components/WatchCategoryCrousal.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
-
-
-import '../assets/css/PhotoGallery.css';
-
-// import carousal from '../assets/images/carousal.jpg'
-
-
-
 import { Link } from 'react-router-dom';
 
+import '../assets/css/WatchCategoryCarousel.css';
 
-const watchData = [
-  
-  { label: '', image: '../assets/images/carousal.jpg', link: '/mens' },
-  { label: '', image: '../assets/images/carousal2.jpg', link: '/womens' },
-  { label: '', image: '../assets/images/carousal3.jpg', link: '/funky' },
-];
+import carousal1 from '../assets/images/carousal.jpg';
+import carousal2 from '../assets/images/carousal2.jpg';
+import carousal3 from '../assets/images/carousal3.jpg';
 
 const WatchCategoryCarousel = () => {
+  const watchData = [
+    { label: 'Men', image: carousal1, link: '/mens' },
+    { label: 'Women', image: carousal2, link: '/womens' },
+    { label: 'Funky', image: carousal3, link: '/funky' },
+  ];
+
   return (
     <div className="carousel-container">
       {watchData.map((item, index) => (
         <Link to={item.link} key={index} className="circle-card-link">
           <div className="circle-card">
             <img src={item.image} alt={item.label || 'Watch'} />
-            {item.label && <span className="label">{item.label}</span>}
+            
           </div>
+          <div className="label text-center mt-2">{item.label}</div>
         </Link>
       ))}
     </div>
